ef3562adb48aae6a55c7cbb68d60737acc0e7ee2
LSM-1020 <smmlee@naver.com>
2025-08-21 14:58:08 +0900

    게시판 하단 선택페이지 추가

--- src/main/java/com/LSM/board/Boardcontroller.java
+++ src/main/java/com/LSM/board/Boardcontroller.java
@@ -31,17 +31,22 @@ public class Boardcontroller extends HttpServlet {
 		int page = 1;
 		//게시판링크 접근할때 처음 페이지는 무조건 1페이지의 내용이 보여야 함
 		//처음에 보여질 페이지의 번호의 초기값을 1로 초기화
+		int totalBoardCount = boardDao.countBoard();//총 글 갯수
 		if(request.getParameter("page") == null) {//참이면 링크를 타고 게시판으로 들어간 경우
 			page = 1;
 		} else { //유저가 보고싶은 페이지번호를 누른 경우
 			page = Integer.parseInt( request.getParameter("page")); //유저가 클릭한 유저가 보고싶어하는 페이지의 번호
 		}
 		
-		
-	
-		
 		List<BoardDto> boardDtos = boardDao.boardList(page);
-		request.setAttribute("boardDtos", boardDtos);
+		int totalPage = (int) Math.ceil((double)totalBoardCount / BoardDao.PAGE_SIZE);
+		
+		request.setAttribute("boardDtos", boardDtos);//유저가 선택한 페이지에 해당하는 글
+		request.setAttribute("currentPage", page);//유저가 현재 선택한 페이지 번호
+		request.setAttribute("totalPage", totalPage);
+		//총 글의 갯수로 표현될 전체 페이지의 수(37개 글이면 4 전달)
+		
+		
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("boardList.jsp");	
 		
